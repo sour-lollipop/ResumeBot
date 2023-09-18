677f479e16494d71e806f28fc78751c831d0e467
sour-lollipop <dossymkhanova.a@gmail.com>
2023-09-18 16:13:37 +0600

    asd

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
@@ -20,6 +19,6 @@ async def cmd_start(message: types.Message):
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
-
+    await bot.delete_webhook(drop_pending_updates=True)
 if __name__ == "__main__":
     asyncio.run(main())
