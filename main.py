0f085017d3a16cf609022eb4c92dd1be8322b65b
sour-lollipop <dossymkhanova.a@gmail.com>
2023-09-18 20:02:26 +0600

    кнопки

--- main.py
+++ main.py
@@ -2,6 +2,25 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+
+class Candidate_States(StatesGroup):
+    Desired_positions = State()
+    Full_name = State()
+    Date_of_birth = State()
+    Place_of_birth = State()
+    Marital_status = State()
+    Have_any_children = State()
+    Height = State()
+    Weight = State()
+    City_of_residence = State()
+    Current_location = State()
+    Nationality = State()
+    Have_travel_passport = State()
+    COVID = State()
+
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
@@ -13,12 +32,21 @@ dp = Dispatcher()
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
-
-
+    lang_kb = InlineKeyboardBuilder()
+    en = types.InlineKeyboardButton(text = 'english', callback_data = 'lang_en')
+    ru = types.InlineKeyboardButton(text = 'русский', callback_data = 'lang_ru')
+    lang_kb.row(ru,en)
+    await message.answer("Выбирите язык / Choose language :",
+                         reply_markup = lang_kb.as_markup()
+                         )
+    
+@dp.callback_query(lambda c: c.data and c.data.startswith('lang'))
+async def choose_lang(callback: types.CallbackQuery):
+    await callback.message.answer(callback.data)
 # Запуск процесса поллинга новых апдейтов
 async def main():
-    await dp.start_polling(bot)
     await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
 if __name__ == "__main__":
     asyncio.run(main())
